e417ecf9c099fac2855e0039b23e08c7aae06add
EThoum <92298486+EThoum@users.noreply.github.com>
2021-10-12 14:36:49 +0200

    HttpRequests

--- cra/src/pages/HttpRequests.js
+++ cra/src/pages/HttpRequests.js
@@ -1,3 +1,27 @@
-export const HttpRequests = () => <>
-    <div>Http Requests</div>
-</>
+import Axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const getCountries = () => Axios.get("https://restcountries.com/v3.1/all")
+
+export const HttpRequests = () => {
+    const [countries, setCountries] = useState([]);
+    useEffect(
+        () => getCountries()
+        .then(
+            results => {
+                console.log('promesse résolue !', results.data);
+                setCountries(results.data);
+            })
+            .catch(error => {
+                alert ('Erreur')
+                console.log(error);
+                throw error;
+            })
+        , []);
+    return <>
+        {
+            countries.length
+        } countries
+    </>
+};
+
