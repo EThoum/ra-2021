3eb9cf9f4c9b82343c7b796b06834f30f36a8a6b
EThoum <92298486+EThoum@users.noreply.github.com>
2021-10-13 15:04:27 +0200

    Form Submit

--- cra/src/pages/Localisation.js
+++ cra/src/pages/Localisation.js
@@ -1,7 +1,7 @@
 import { IntlProvider, FormattedMessage } from 'react-intl';
 import Fr from '../translations/fr.json';
 import En from '../translations/en.json';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 console.log(navigator.language);
 const locale = navigator.language;
@@ -11,12 +11,35 @@ const translationEnum = {
 }
 
 
+
 export const Localisation = () => {
-    const [currentValue, setCurrentValue] = useState('');
+    const [currentValue, setCurrentValue] = useState({
+        nom: 'xxx',
+        prenom: 'yyyyy'
+    });
 
     const onTextChange = ({ target: { value } }) => {
         setCurrentValue(value);
     }
+
+    const onFormSubmit = form => {
+        form.preventDefault();
+        const formData = new FormData(form.target);
+        localStorage.setItem('nom', formData.get("nom"));
+        localStorage.setItem('prenom', formData.get("prenom"));
+        setCurrentValue({
+            nom: localStorage.getItem("nom"),
+            prenom: localStorage.getItem("prenom")
+        })
+    }
+
+    useEffect(() => {
+        setCurrentValue({
+            nom: localStorage.getItem("nom"),
+            prenom: localStorage.getItem("prenom")
+        })
+    }, []);
+
     return <IntlProvider
         messages={translationEnum[locale]}
         {...{ locale }}
@@ -37,10 +60,17 @@ export const Localisation = () => {
             id="toto"
             defaultMessage="toto par défaut" />
 
-        <h4>{currentValue}</h4>
-
-        <form>
-            <input type="text" onChange={onTextChange} />
+        <h4>{currentValue.nom} {currentValue.prenom}</h4>
+        <form onSubmit={onFormSubmit}>
+            <div className="p-3">
+                <input type="text" name="nom" className="form-control" />
+            </div>
+            <div className="p-3">
+                <input type="text" name="prenom" className="form-control" />
+            </div>
+            <div className="p-3">
+                <button className="btn btn-warning">Submit</button>
+            </div>
         </form>
 
     </IntlProvider>
