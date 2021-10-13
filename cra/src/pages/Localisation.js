06a51c758c737692abb7e2581e05ce7f9f6f2d29
EThoum <92298486+EThoum@users.noreply.github.com>
2021-10-13 14:38:02 +0200

    form onchange

--- cra/src/pages/Localisation.js
+++ cra/src/pages/Localisation.js
@@ -1,6 +1,7 @@
 import { IntlProvider, FormattedMessage } from 'react-intl';
 import Fr from '../translations/fr.json';
 import En from '../translations/en.json';
+import { useState } from 'react';
 
 console.log(navigator.language);
 const locale = navigator.language;
@@ -9,24 +10,39 @@ const translationEnum = {
     "en-US": En
 }
 
-export const Localisation = () => <IntlProvider
-    messages={translationEnum[locale]}
-    {...{ locale }}
-    defaultLocale="en">
 
-    <h1>Localisation</h1>
-    <FormattedMessage
-        id="myMessage"
-        defaultMessage="Today is {ts, date, :::yyyyMMdd}"
-        values={{ ts: Date.now() }} />
-    <br />
-    <FormattedMessage
-        id="helloReact"
-        defaultMessage="Hello {nom}"
-        values={{ nom: "Elo" }} />
-    <br />
-    <FormattedMessage
-        id="toto"
-        defaultMessage="toto par défaut" />
-</IntlProvider>;
+export const Localisation = () => {
+    const [currentValue, setCurrentValue] = useState('');
+
+    const onTextChange = ({ target: { value } }) => {
+        setCurrentValue(value);
+    }
+    return <IntlProvider
+        messages={translationEnum[locale]}
+        {...{ locale }}
+        defaultLocale="en">
+
+        <h1>Localisation</h1>
+        <FormattedMessage
+            id="myMessage"
+            defaultMessage="Today is {ts, date, :::yyyyMMdd}"
+            values={{ ts: Date.now() }} />
+        <br />
+        <FormattedMessage
+            id="helloReact"
+            defaultMessage="Hello {nom}"
+            values={{ nom: "Elo" }} />
+        <br />
+        <FormattedMessage
+            id="toto"
+            defaultMessage="toto par défaut" />
+
+        <h4>{currentValue}</h4>
+
+        <form>
+            <input type="text" onChange={onTextChange} />
+        </form>
+
+    </IntlProvider>
+};
 
